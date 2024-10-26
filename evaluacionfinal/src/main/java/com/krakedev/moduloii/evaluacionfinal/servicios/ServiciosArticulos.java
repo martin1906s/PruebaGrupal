5a0a1e9802e419a6a4378ac8a7bb1e901eb9316a
Josema <josemaleoncarrera@gmail.com>
2024-10-26 17:35:08 -0500

    Servicio insertar articulo

--- evaluacionfinal/src/main/java/com/krakedev/moduloii/evaluacionfinal/servicios/ServiciosArticulos.java
+++ evaluacionfinal/src/main/java/com/krakedev/moduloii/evaluacionfinal/servicios/ServiciosArticulos.java
@@ -1,5 +1,31 @@
 package com.krakedev.moduloii.evaluacionfinal.servicios;
 
-public class ServiciosArticulos {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.krakedev.excepciones.KrakeDevExcepcion;
+import com.krakedev.moduloii.evaluacionfinal.entidades.Articulo;
+import com.krakedev.moduloii.evaluacionfinal.persistencia.ArticulosBDD;
+
+
+@Path("articulo")
+public class ServiciosArticulos {
+	@Path("insertar")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response insertar(Articulo articulo) {
+    	System.out.println(">>>>>>>>>"+articulo);
+    	ArticulosBDD art = new ArticulosBDD();
+    	try {
+			art.inertar(articulo);
+			return Response.ok().build();
+		} catch (KrakeDevExcepcion e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+    	
+    }
 }
